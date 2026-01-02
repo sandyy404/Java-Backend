de9ff7f8d3cc77ddc172c92db01ef09ee6657321
sandyy404 <sp8251960@gmail.com>
2026-01-02 14:45:45 +0530

    updated

---
+++ Advance/aab_inn_anon.java
@@ -0,0 +1,18 @@
+package Advance;
+
+abstract class A {
+
+    public abstract void show();
+}
+
+public class aab_inn_anon {
+
+    public static void main(String[] args) {
+        A obj = new A() {
+            public void show() {
+                System.out.println("This is overring the abstract class method.");
+            }
+        };
+        obj.show();
+    }
+}
