ccfcc86acaece68a254e8631e74b9ad1f31ad731
sandyy404 <sp8251960@gmail.com>
2026-01-03 15:42:24 +0530

    updated

--- Advance/Enum.java
+++ Advance/Enum.java
@@ -1,18 +1,40 @@
 package Advance;
 
-
-enum Status{
-   Success,Failed,Pending;
+enum Status {
+    Success, Failed, Pending;    // these are the object constant of the Status class
 }
-public class Enum {
-   public static void main(String[] args) {
-      Status s =  Status.Success;
-      // System.out.println(s);
-      // System.out.println(s.ordinal());   // this will give the order of s.
-      Status []ss = Status.values();
-     for(Status a :ss){
-      System.out.println(a.ordinal() + ":" + a);
-     }
 
-   }
+public class Enum {
+
+    public static void main(String[] args) {
+        Status s = Status.Failed;
+        // System.out.println(s);
+        // System.out.println(s.ordinal());   // this will give the order of s.
+        //    Status []ss = Status.values();
+        //   for(Status a :ss){
+        //    System.out.println(a.ordinal() + ":" + a);
+        //   }
+
+        // if(s==Status.Failed){
+        //    System.out.println("Sorry! Try again" );
+        // }else if(s == Status.Pending){
+        //    System.out.println("Your transaction is underprocess");
+        // }else if(s == Status.Success){
+        //    System.out.println("Congratulation! Your transaction is successfully done");
+        // }
+        switch (s) {
+            case Success:
+                System.out.println("Congratulation! Your transaction is successfully done");
+                break;
+            case Failed:
+                System.out.println("Sorry! Try again");
+                break;
+            case Pending:
+                System.out.println("Your transaction is underprocess");
+                break;
+            default:
+                System.out.println("Done");
+        }
+
+    }
 }
