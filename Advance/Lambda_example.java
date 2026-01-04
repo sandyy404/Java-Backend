9e694dc76b2cf00f6e991f6d66ed186941f08e75
sandyy404 <sp8251960@gmail.com>
2026-01-05 01:50:56 +0530

    updated

--- Advance/Lambda_example.java
+++ Advance/Lambda_example.java
@@ -1,5 +1,18 @@
 package Advance;
 
+@FunctionalInterface
+interface A{
+   int sum(int i,int j);
+}
 public class Lambda_example {
-   
+   public static void main(String[] args) {
+      // A jod = new A() {
+      //   public  int sum(int i,int j){
+      //    return i+j;
+      //   }
+      // };
+      A jod = (int i,int j) -> i+j;
+     int a = jod.sum(5,4);
+     System.out.println(a);
+   }
 }
